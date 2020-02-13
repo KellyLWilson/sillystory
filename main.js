b4ca1f8157244ecd201d0c16ef204ad072c0c812
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-02-13 16:44:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
